fef632e28e0a2e9268b7c620ee02263c9b7e7d8c
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-16 09:48:41 +0100

    make exo 60

---
+++ exo 1/app.js
@@ -0,0 +1,11 @@
+
+
+let items = document.getElementsByTagName("li");
+let a = 0;
+
+while (a < 7) {
+    console.log("A vaut actuellement " + a);
+    a++;
+    items[a].style.listStyleType = "circle";
+    items[a].innerHTML = "blablabla";
+}
